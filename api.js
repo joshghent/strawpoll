8f6303d9cebee340394abde5333934f0f626880f
Josh Ghent <me@joshghent.com>
2016-10-16 11:48:37 +0100

    Made tabs worth 2 spaces for eslint config. Added app.get route for incoming poll redirects.

--- api.js
+++ api.js
@@ -1,7 +1,33 @@
-var mongo = require('mongodb').MongoClient;
+const mongo = require('mongodb').MongoClient;
 
-module.exports = function (app) {
-    mongo.connect(String(process.env.MONGOHQ_URL), function (err, db) {
-        
-    })
-}
+let polls;
+
+module.exports = (app) => {
+  mongo.connect(String(process.env.MONGOHQ_URL), (err, db) => {
+    polls = db.collection('strawpoll');
+  });
+
+  // Urls to polls will always be something like 'strawpoll.joshghent.com/poll/<RANDOM 8 CHAR STRING>'
+  app.get(/^\/poll\/\w+$/, (req, res) => {
+    const requestedPoll = req.url.split('/').pop();
+
+    if (requestedPoll) {
+
+      // Find the poll at the requested url in the database
+      polls.findOne({ poll: requestedPoll }, (err, data) => {
+
+        // If we find the poll then post back the json data for it
+        if (data) {
+          res.json(data);
+   
+        // Otherwise we output that we couldn't find a poll
+        } else {
+          res.json({ message: "No poll found" });
+        }
+      });
+    }
+  });
+
+  app.post('/save', (req, res) => {
+  });
+};
--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app)
-var port = process.env.PORT || 5432;
-var shortid = require('shortid');
-var mongod = require('mongodb').MongoClient;
-var path = require('path');
-var api = require('/api.js');
+const port = process.env.PORT || 5432;
+
+const express = require('express');
+
+const app = express();
+
+const http = require('http').Server(app);
+const path = require('path');
+const api = require('/api.js');
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/createPoll/createPoll.html');
 });
 
 api(app);
 
-http.listen(port, function () {
-    console.log("Listening on: " + port);
+http.listen(port, () => {
+  console.log("Listening on: " + port);
 });
